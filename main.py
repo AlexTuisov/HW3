bacd0fecf032ffdc69b4f681ce8a3f4420571ee3
Alex Tuisov <queldelan@gmail.com>
2021-01-04 20:04:54 +0200

    continued work

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import time
 CONSTRUCTOR_TIMEOUT = 60
 ACTION_TIMEOUT = 5
 
+
+def state_to_agent(state):
+    pass
+
 class Game:
     def __init__(self):
         self.initial_state = None
@@ -41,10 +45,30 @@ class Game:
         return action
 
     def check_if_action_legal(self, action):
+        location, effect = action[1], action[0]
+        try:
+            status = self.state[location]
+        except KeyError:
+            raise
+        if effect.lower() not in ['vaccinate', 'quarantine']:
+            return False
+
+    def apply_actions(self, actions):
+        for action in actions:
+            location, effect = action[1], action[0]
+            if 'v' in effect:
+                self.state[location] = 'I'
+            else:
+                self.state[location] = 'Q0'
+
+    def change_dynamics(self):
         pass
 
-    def apply_action(self, action):
-        self.check_if_action_legal(action)
+    def update_scores(self):
+        pass
+
+    def handle_illegal_action(self, agent):
+        pass
 
     def handle_action_timeout(self, agent):
         pass
@@ -55,15 +79,22 @@ class Game:
     def play_game(self):
         agent_1, agent_2 = self.initiate_agents(self.control_zone_1, self.control_zone_2)
         self.play_episode(agent_1, agent_2)
+        self.state = deepcopy(self.initial_state)
         agent_1, agent_2 = self.initiate_agents(self.control_zone_2, self.control_zone_1)
         self.play_episode(agent_1, agent_2)
         return self.score
-        
 
     def play_episode(self, agent_1, agent_2):
         while 'S' in self.state.values():
             action_1 = self.get_action(agent_1)
+            if not self.check_if_action_legal(action_1):
+                self.handle_illegal_action(agent_1)
             action_2 = self.get_action(agent_2)
+            if not self.check_if_action_legal(action_2):
+                self.handle_illegal_action(agent_2)
+            self.apply_actions([action_1, action_2])
+            self.change_dynamics()
+            self.update_scores()
 
 
 def main():
@@ -73,7 +104,6 @@ def main():
         print(f'Score for {agent1.ids} is {results[0]}, score for {agent2.ids} is {results[1]}')
     except Exception as e:
         print(e)
-
 
 
 if __name__ == '__main__':
