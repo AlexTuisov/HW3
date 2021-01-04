8c1da289a467556a1349dd195c4b49d591b00c11
AlexTuisov <Queldelan@gmail.com>
2021-01-04 08:53:59 +0200

    added stuff

--- agent2.py
+++ agent2.py
@@ -1,3 +1,5 @@
+ids = ['111111111', '111111111']
+
 class Agent:
     def __init__(self, zone_of_control):
         print(f'hi, I am agent 2')
--- main.py
+++ main.py
@@ -8,26 +8,28 @@ ACTION_TIMEOUT = 5
 
 class Game:
     def __init__(self):
-        self.state = None
+        self.initial_state = None
         self.control_zone_1 = None
         self.control_zone_2 = None
-        self.agent_1 = None
-        self.agent_2 = None
+        self.score = [0, 0]
         self.divide_map()
-        self.initiate_agents()
+        self.state = deepcopy(self.initial_state)
 
-    def initiate_agents(self):
+    def initiate_agents(self, control_zone_1, control_zone_2):
         start = time.time()
-        self.agent_1 = agent1.Agent(self.control_zone_1)
+        agent_1 = agent1.Agent(control_zone_1)
         if time.time() - start > CONSTRUCTOR_TIMEOUT:
-            self.handle_constructor_timeout(self.agent_1)
+            self.handle_constructor_timeout(agent_1)
+
         start = time.time()
-        self.agent_2 = agent2.Agent(self.control_zone_2)
+        agent_2 = agent2.Agent(control_zone_2)
         if time.time() - start > CONSTRUCTOR_TIMEOUT:
-            self.handle_constructor_timeout(self.agent_2)
+            self.handle_constructor_timeout(agent_2)
+        
+        return agent_1, agent_2
 
     def divide_map(self):
-        self.state = None
+        self.initial_state = None
         self.control_zone_1 = None
         self.control_zone_2 = None
 
@@ -50,14 +52,28 @@ class Game:
     def handle_constructor_timeout(self, agent):
         pass
 
-    def play_episode(self):
+    def play_game(self):
+        agent_1, agent_2 = self.initiate_agents(self.control_zone_1, self.control_zone_2)
+        self.play_episode(agent_1, agent_2)
+        agent_1, agent_2 = self.initiate_agents(self.control_zone_2, self.control_zone_1)
+        self.play_episode(agent_1, agent_2)
+        return self.score
+        
+
+    def play_episode(self, agent_1, agent_2):
         while 'S' in self.state.values():
-            action_1 = self.get_action(self.agent_1)
-            action_2 = self.get_action(self.agent_2)
+            action_1 = self.get_action(agent_1)
+            action_2 = self.get_action(agent_2)
 
 
 def main():
     game = Game()
+    try:
+        results = game.play_game()
+        print(f'Score for {agent1.ids} is {results[0]}, score for {agent2.ids} is {results[1]}')
+    except Exception as e:
+        print(e)
+
 
 
 if __name__ == '__main__':
